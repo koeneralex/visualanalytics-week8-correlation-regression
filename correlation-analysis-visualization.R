af6774ac93713f39e75a818946c47b65aadcae23
Alex Koener <77310205+koeneralex@users.noreply.github.com>
2021-03-04 17:13:27 -0500

    Add files via upload

--- correlation-analysis-visualization.R
+++ correlation-analysis-visualization.R
@@ -1,11 +1,11 @@
-## Instructions ----------------
+## Instructions --------------------
 
   # Create your own visual analytics based on correlation or regression analysis using ggplot2.
   
   # The visual will follow our textbook recommendation to use grid to enhance the comparisons between scatter plots or your variables.
 
 
-## Libraries -------------------
+## Libraries -----------------------
 
 library(datasets) # Data sets
 library(tidyverse) # Ggplot/dplyr
@@ -46,7 +46,20 @@ qplot(x=Var1, y=Var2, fill=value, data=dfCor, geom="tile") +
   labs(title="US Arrests Correlation Analysis", x="Variable 1", y="Variable 2")
 
 
+## Analysis -----------------------
+
+  # Calculated the correlation between rapes committed and the urban population.
+    # R-squared  = 0.1519
+    # This number is close to zero showing little correlation.
+
+  # After, I created a regression model based on the same variables to predict values in the future based on the urban population.
+    # UrbanPop = 100; Rapes = 30.4043
+    # UrbanPop = 200; Rapes = 57.02152
+
+  # Plot urban population (independent variable) on x-axis and Rape (dependent variable) on y-axis to visualize relationship.
+
+  # Additional plot to demonstrate correlations on a tile map.
+    # Highest correlations being between Assault and Murder.
 
 
-
-
+  # GitHub: (https://github.com/koeneralex/visualanalytics-week8-correlation-regression)
